04794ae97743479b96ad6a1750d65ed5107cd2f8
jnko <joern.koerner@gmail.com>
2016-10-12 08:12:35 +0200

    Add files via upload

--- SimpleDatabaseBackup/SimpleDatabaseBackup.cs
+++ SimpleDatabaseBackup/SimpleDatabaseBackup.cs
@@ -36,7 +36,7 @@ namespace SimpleDatabaseBackup
 
         public override string UpdateUrl
         {
-            get { return "https://raw.githubusercontent.com/jnko/SimpleDatabaseBackup/master/SimpleDatabaseBackupVERSION.txt"; }
+            get { return @"http://raw.githubusercontent.com/jnko/SimpleDatabaseBackup/master/SimpleDatabaseBackupVERSION.txt"; }
         }
 
         public override bool Initialize(IPluginHost host)
